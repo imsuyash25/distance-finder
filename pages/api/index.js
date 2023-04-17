77851086676ab3c014fca4b3a99d160f92f54435
imsuyash25 <asatisuyash@gmail.com>
2023-04-17 16:09:48 +0530

    project created

---
+++ pages/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": "270571403fmshc32177e2bfa749dp1d8358jsn98c757732960",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error : ${error}`);
+  }
+};
